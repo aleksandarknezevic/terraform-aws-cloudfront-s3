efc008b67e622935b91474e6188dcdff64eb8520
aleksandarknezevic <perun.84@gmail.com>
2021-05-31 11:24:34 +0200

    outputs fix

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "bucket_name" {
   depends_on = [aws_s3_bucket.bucket]
-  value = aws_s3_bucket.bucket.bucket
+  value = aws_s3_bucket.bucket[count.index].bucket
 }
 
 output "bucket_arn" {
   depends_on = [aws_s3_bucket.bucket]
-  value = aws_s3_bucket.bucket.arn
+  value = aws_s3_bucket.bucket[count.index].arn
 }
 
 output "bucket_acl" {
   depends_on = [aws_s3_bucket.bucket]
-  value = aws_s3_bucket.bucket.acl
+  value = aws_s3_bucket.bucket[count.index].acl
 }
 
 output "bucket_domain_name" {
   depends_on = [aws_s3_bucket.bucket]
-  value = aws_s3_bucket.bucket.bucket_domain_name
+  value = aws_s3_bucket.bucket[count.index].bucket_domain_name
 }
