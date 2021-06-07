5a50d85794d17abfe7e7248bbfa575d45d7b0636
aleksandarknezevic <perun.84@gmail.com>
2021-06-07 11:56:56 +0200

    test additional provider

--- versions.tf
+++ versions.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = "~> 0.15.0"
 
   required_providers {
-    aws = ">= 3.40 "
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.40.0"
+      configuration_aliases = [ aws.virginia ]
+    }
   }
 }
