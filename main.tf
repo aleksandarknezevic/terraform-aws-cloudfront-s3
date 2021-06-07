d18c6e85fdbe00c0adac229c2775be7876fd8df8
aleksandarknezevic <perun.84@gmail.com>
2021-06-07 19:55:25 +0200

    provider in validation

--- main.tf
+++ main.tf
@@ -107,6 +107,7 @@ resource "aws_acm_certificate" "certificate" {
 
 ## SSL certificate validation
 resource "aws_route53_record" "validation" {
+  provider = aws.virginia
   for_each = {
     for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -125,6 +126,7 @@ resource "aws_route53_record" "validation" {
 }
 
 resource "aws_acm_certificate_validation" "validation" {
+  provider = aws.virginia
   certificate_arn         = aws_acm_certificate.certificate.arn
   validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
   depends_on = [aws_acm_certificate.certificate]
